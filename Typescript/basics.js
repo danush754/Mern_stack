5939759e2b8e0f8c176377822adfc443f01b031f
Dhanush <danush2228s@gmail.com>
2023-10-17 17:54:28 +0530

    New Folder for practising typescript

---
+++ Typescript/basics.js
@@ -0,0 +1,8 @@
+var productId = 1045;
+var productDesc = "16 GB, Gold";
+var productName = "Samsung S20";
+var productAvailability = true;
+console.log(productId + "is the type of " + typeof productId);
+console.log(productDesc + "is the type of " + typeof productDesc);
+console.log(productName + "is the type of " + typeof productName);
+console.log(productAvailability + "is the type of " + typeof productAvailability);
---
+++ Typescript/basics.ts
@@ -0,0 +1,8 @@
+let productId:number = 1045;
+let productDesc:string ="16 GB, Gold";
+let productName:string = "Samsung S20";
+let productAvailability:boolean  = true;
+console.log(productId + "is the type of "+ typeof productId)
+console.log(productDesc + "is the type of "+ typeof productDesc)
+console.log(productName + "is the type of "+ typeof productName)
+console.log(productAvailability + "is the type of "+ typeof productAvailability)
